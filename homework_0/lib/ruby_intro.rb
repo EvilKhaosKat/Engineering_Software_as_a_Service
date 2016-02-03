13fa7c72e799e4aca4dd23fc58eb99e201299857
EvilKhaosKat <evilkhaoskat@gmail.com>
2016-02-03 22:01:16 +0400

    sum_to_n? task implemented

--- homework_0/lib/ruby_intro.rb
+++ homework_0/lib/ruby_intro.rb
@@ -5,7 +5,7 @@
 # Define a method sum(array) that takes an array of integers as an argument
 # and returns the sum of its elements.
 # For an empty array it should return zero.
-def sum arr
+def sum(arr)
   return 0 if arr.empty?
 
   arr.reduce(:+)
@@ -15,7 +15,7 @@ end
 # which takes an array of integers as an argument and returns the sum of its two largest elements.
 # For an empty array it should return zero.
 # For an array with just one element, it should return that element.
-def max_2_sum arr
+def max_2_sum(arr)
   return 0 if arr.empty?
   return arr[0] if arr.size == 1
 
@@ -31,8 +31,13 @@ end
 # Define a method sum_to_n?(array, n) that takes an array of integers and an additional integer, n, as arguments
 # and returns true if any two elements in the array of integers sum to n.
 # sum_to_n?([], n) should return false for any value of n, by definition.
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  return false if arr.empty?
+
+  combinations = arr.combination(2).to_a # for [1, 2, 3] return [[1, 2], [1, 3], [2, 3]]
+  sums = combinations.map { |combination| combination.inject(0, :+) }
+
+  sums.include? n
 end
 
 # Part 2
