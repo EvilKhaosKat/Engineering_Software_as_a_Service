637b6181a74313162550d50268d5f25decf36ad4
EvilKhaosKat <evilkhaoskat@gmail.com>
2016-02-04 21:26:10 +0400

    consolants

--- homework_0/lib/ruby_intro.rb
+++ homework_0/lib/ruby_intro.rb
@@ -50,14 +50,23 @@ end
 # and returns true if it starts with a consonant and false otherwise.
 # (For our purposes, a consonant is any letter other than A, E, I, O, U.)
 # NOTE: be sure it works for both upper and lower case and for nonletters!
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  first_symbol = s[0]
+  return false if first_symbol.nil?
+
+  if %w(a e i o u A E I O U).include? first_symbol
+    false # vowels
+  elsif ('a'..'z').include? first_symbol.downcase
+    true #consolants
+  else
+    false #nonletters
+  end
 end
 
 # Define a method binary_multiple_of_4?(s) that takes a string
 # and returns true if the string represents a binary number that is a multiple of 4.
 # NOTE: be sure it returns false if the string is not a valid binary number!
-def binary_multiple_of_4? s
+def binary_multiple_of_4?(s)
   # YOUR CODE HERE
 end
 
